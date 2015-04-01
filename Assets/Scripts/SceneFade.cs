d92f5011dac097fd8b8fda20b81a63a30b13bd97
Michael Miles <boat081062@gmail.com>
2015-04-01 17:54:32 -0500

    No video today. Worked myself.
    Today I updated and created a Fade in and out script taking advantage of
    the new UI and different techniques I learned in the other game
    tutorials. My script is substantially smaller than the original.

---
+++ Assets/Scripts/SceneFade.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SceneFade : MonoBehaviour
+{
+    // Declaration of public variables
+
+
+    // Declaration of private variables
+    private bool sceneStarting = true;
+    private Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+       if(sceneStarting)
+       {
+           StartScreen();
+       }
+
+    }
+    
+    void FadeToClear()
+    {
+        anim.SetTrigger("StartScene");
+    }
+
+    void FadeToBlack()
+    {
+        anim.SetTrigger("EndScene");
+    }
+
+    void StartScreen()
+    {
+        FadeToClear();
+        sceneStarting = false;
+    }
+
+    public void EndScene()
+    {
+        FadeToBlack();
+        Application.LoadLevel(1);
+    }
+}
