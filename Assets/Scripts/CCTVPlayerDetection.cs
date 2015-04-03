f586899c9d336120e3d5cd3161e8b068f6b51269
Michael Miles <boat081062@gmail.com>
2015-04-03 05:19:01 -0500

    Video 107
    Completed the CCTV cameras, script and animation. About half of the
    script had to be converted for Unity 5 and the entire animation was
    changed to use Mecanim.

---
+++ Assets/Scripts/CCTVPlayerDetection.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CCTVPlayerDetection : MonoBehaviour
+{
+    // Declaration of public variables
+
+    // Declaration of private variables
+    private GameObject player;
+    private LastPlayerSighting lastPlayerSighting;
+
+    // Awake is called when the script instance is being loaded
+    public void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag(Tags.player);
+        lastPlayerSighting = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<LastPlayerSighting>();
+    }
+
+    // OnTriggerStay is called once per frame for every Collider other that is touching the trigger
+    void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            Vector3 relPlayerPos = player.transform.position - transform.position;
+            RaycastHit hit;
+
+            if(Physics.Raycast(transform.position, relPlayerPos, out hit))
+            {
+                if(hit.collider.gameObject == player)
+                {
+                    lastPlayerSighting.position = player.transform.position;
+                }
+            }
+        }
+    }
+}
