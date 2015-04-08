8b2540fe044576793320d0497742d6a2a396250b
Michael Miles <boat081062@gmail.com>
2015-04-08 05:22:59 -0500

    Video 302
    Keycard animation, pickup sounds and pickup. A lot of this game will be
    used in my hotel game.

---
+++ Assets/Scripts/LevelDynamics/KeyPickup.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyPickup : MonoBehaviour
+{
+    // Declaration of public variables
+    public AudioClip keyGrab;
+
+    // Declaration of private variables
+    private GameObject player;
+    private PlayerInventory playerInventory;
+
+    // Awake is called when the script instance is being loaded
+    public void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag(Tags.player);
+        playerInventory = player.GetComponent<PlayerInventory>();
+    }
+
+    // OnTriggerEnter is called when the Collider other enters the trigger
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            AudioSource.PlayClipAtPoint(keyGrab, transform.position);
+            playerInventory.hasKey = true;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInventory : MonoBehaviour
+{
+    // Declaration of public variables
+    public bool hasKey;
+}
